94800fb4caa845d99785b09518ae00531078454a
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2021-10-27 01:08:30 +0100

    Removed unused import.

--- ggc/core/WarehouseManager.java
+++ ggc/core/WarehouseManager.java
@@ -2,7 +2,6 @@ package ggc.core;
 
 //FIXME import classes (cannot import from pt.tecnico or ggc.app)
 
-import java.io.Serializable;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
