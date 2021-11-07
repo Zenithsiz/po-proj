d1869ae70c9febd3412fce121dad36f3ee18112a
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2021-11-07 20:27:31 +0000

    Removed `Result`.

--- ggc/core/util/Pair.java
+++ ggc/core/util/Pair.java
@@ -1,13 +1,7 @@
 package ggc.core.util;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
 import java.util.function.Function;
-import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -55,74 +49,6 @@ public class Pair<L, R> {
 
 	/// Returns a collector to use with streams of type `Pair`
 	public static <Lhs, Rhs> Collector<Pair<Lhs, Rhs>, ?, Map<Lhs, Rhs>> toMapCollector() {
-		return Collectors.<Pair<Lhs, Rhs>, Lhs, Rhs>toMap(pair -> pair._lhs, pair -> pair._rhs);
-	}
-
-	/// Returns a map collector that returns empty if any of the keys were empty
-
-	/// Returns a map collector that returns `Result.Err` if any of the keys were an error
-	public static <Lhs, Rhs> Collector<Pair<Result<Lhs>, Rhs>, ?, Result<Map<Lhs, Rhs>>> toResultMapCollector() {
-		return new ResultMapCollectorImpl<>();
-	}
-
-	/// Collector for `toResultMapCollector`
-	private static class ResultMapCollectorImpl<Lhs, Rhs> implements
-			Collector<Pair<Result<Lhs>, Rhs>, ResultMapCollectorImpl.ResultHashMap<Lhs, Rhs>, Result<Map<Lhs, Rhs>>> {
-		@Override
-		public Supplier<ResultHashMap<Lhs, Rhs>> supplier() {
-			return () -> new ResultHashMap<Lhs, Rhs>();
-		}
-
-		@Override
-		public BiConsumer<ResultHashMap<Lhs, Rhs>, Pair<Result<Lhs>, Rhs>> accumulator() {
-			return (resMap, pair) -> {
-				// If the map is an error, don't do anything
-				if (resMap.map.isErr()) {
-					return;
-				}
-
-				// If the pair key is an error, set the map to the error and return
-				var pairLhsErr = pair.getLhs().<HashMap<Lhs, Rhs>>mapWithErr();
-				if (pairLhsErr.isPresent()) {
-					resMap.map = pairLhsErr.get();
-					return;
-				}
-
-				// Else add the element
-				resMap.map.getOk().put(pair.getLhs().getOk(), pair.getRhs());
-			};
-		}
-
-		@Override
-		public BinaryOperator<ResultHashMap<Lhs, Rhs>> combiner() {
-			return (lhsMap, rhsMap) -> {
-				// If any of the maps are an error, return the error
-				if (lhsMap.map.isErr()) {
-					return lhsMap;
-				}
-				if (rhsMap.map.isErr()) {
-					return rhsMap;
-				}
-
-				// Else merge all rhs into lhs and return it
-				lhsMap.map.getOk().putAll(rhsMap.map.getOk());
-				return lhsMap;
-			};
-		}
-
-		@Override
-		public Function<ResultHashMap<Lhs, Rhs>, Result<Map<Lhs, Rhs>>> finisher() {
-			return resMap -> resMap.map.map(map -> map);
-		}
-
-		@Override
-		public Set<Characteristics> characteristics() {
-			return Collections.emptySet();
-		}
-
-		/// Helper class to contain a `Result<HashMap<Lhs, Rhs>>` so we can set it to `Err` in the functions above
-		private static class ResultHashMap<Lhs, Rhs> {
-			public Result<HashMap<Lhs, Rhs>> map = Result.ofOk(new HashMap<>());
-		}
+		return Collectors.<Pair<Lhs, Rhs>, Lhs, Rhs>toMap(Pair::getLhs, Pair::getRhs);
 	}
 }
--- ggc/core/util/ThrowableSupplier.java
+++
@@ -1,8 +0,0 @@
-package ggc.core.util;
-
-/// A supplier that may throw
-@FunctionalInterface
-public interface ThrowableSupplier<T, E extends Throwable> {
-	/// Gets the result, or throws
-	T get() throws E;
-}
